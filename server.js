a7a7a940ae24f51c3acfb4fbaaad09443ea83261
Traiblaze <alika_fr_anthony2010@rocketmail.com>
2021-12-10 18:59:36 +0000

    Project Modified

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors'); 
-const config = require('./config');
 
 const app = express();
 
 
-
-app.use(express.urlencoded({extended: true})); // Parse URL-encoded bodies
-app.use(express.json());
-app.use(cors());
-app.use(express.static('./dist/com-project'));
-
+app.use(express.static(__dirname + '/dist/com-project'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: '/dist/com-project/'});
+    res.sendFile(path.join(__dirname + '/dist/com-project/index.html'));
 });
 
-app.listen(process.env.PORT || "3000");
+
+app.listen(process.env.PORT || 3000);
