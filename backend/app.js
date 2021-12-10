d192a93dfc6acd4a6cbbcc8fbebc94461b444938
Traiblaze <alika_fr_anthony2010@rocketmail.com>
2021-12-10 16:37:42 +0000

    Backend issue fixed

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,10 @@ const app = express();
 app.use(express.urlencoded({extended: true})); // Parse URL-encoded bodies
 app.use(express.json());
 app.use(cors());
-app.use(express.static(__dirname + '/dist/com-project'));
+app.use(express.static('./dist/com-project'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/com-project/index.html'));
+  res.sendFile('index.html', {root: '/dist/com-project/'});
 });
 
 
