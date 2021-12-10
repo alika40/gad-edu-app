9ef4464b66692b092c702b73993a68faea4fae48
Traiblaze <alika_fr_anthony2010@rocketmail.com>
2021-12-10 18:34:57 +0000

    app.js removed

--- server.js
+++ server.js
@@ -1,52 +1,19 @@
-const app = require("./Backend/app");
-const debug = require("debug")("node-angular");
-const http = require("http");
+const express = require('express');
+const path = require('path');
+const cors = require('cors'); 
+const config = require('./config');
 
-const normalizePort = val => {
-  var port = parseInt(val, 10);
+const app = express();
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
 
-  return false;
-};
+app.use(express.urlencoded({extended: true})); // Parse URL-encoded bodies
+app.use(express.json());
+app.use(cors());
+app.use(express.static('./dist/com-project'));
 
-const onError = error => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: '/dist/com-project/'});
+});
 
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  debug("Listening on " + bind);
-};
-
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
-
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+app.listen(process.env.PORT || "3000");
