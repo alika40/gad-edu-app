bd9b2b3b64efcebaf92fcbf9dc2e9f66f111e549
Traiblaze <alika_fr_anthony2010@rocketmail.com>
2022-01-03 15:24:48 +0000

    Not Important

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_URL: 'https://eschool-edu.herokuapp.com/api',
+  api_URL: 'https://gad-edu-app.herokuapp.com/api',
+  api_URL2:  'https://eschool-edu.herokuapp.com/api',
 };
